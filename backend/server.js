ed39aeddacfc157578062e7b898d56deaf146899
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-25 19:05:18 +0500

    Add database connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const app = require('./app');
 
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 // Config
 dotenv.config({path: "backend/config/config.env"});
+
+// Connect to the database
+connectDatabase();
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`server started at: http://localhost:${process.env.PORT}`);
