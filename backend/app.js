04e52c339a48a42b336c16e9d590ff8b54091783
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-25 18:30:19 +0500

    Setup get all products route

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
+
+// Route Imports
+const products = require('./routes/productRoute');
+
+app.use("/api/v1", products);
 
 module.exports = app;
