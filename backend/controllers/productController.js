9f5f2f6c9b3340a809601fa20ab2f585b998cc32
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-26 20:49:14 +0500

    Add controller and route to create new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,17 @@
+const Product = require('../models/productModel');
+
+
+// Create product
+exports.createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  
+  res.status(201).json({
+    success:true,
+    product
+  });
+}
+
+
 exports.getAllProducts = (req, res) => {
 
   res.status(200).json({ message: "Route is working..." });
