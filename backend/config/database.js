f668bcffc53ed5af89775b3797b723a6db60d571
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-26 20:32:22 +0500

    Add product Schema and model

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const connectDatabase = () => {
   mongoose.connect(process.env.DB_URI, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    useCreateIndex:true
+    // useCreateIndex:true
   }).then((data) => {
     console.log(`Mongodb connected with server ${data.connection.host}`)
   }).catch((err) => {
