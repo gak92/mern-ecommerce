4cdce93f5eeaa2493a48ceaafbbee46f2e59ce6f
Ghazanfar Ali <gak_92@hotmail.com>
2023-02-27 11:45:24 +0500

    Add controller and route for the product update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,3 +21,28 @@ exports.getAllProducts = async (req, res) => {
     products  
   });
 };
+
+
+// Update product -- ADMIN
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if(!product) {
+    return res.status(500).json({
+      success:false,
+      message:"Product not found"
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new:true,
+    runValidators:true,
+    useFindAndModify:false
+  });
+
+  res.status(200).json({
+    success:true,
+    product
+  })
+}
+
